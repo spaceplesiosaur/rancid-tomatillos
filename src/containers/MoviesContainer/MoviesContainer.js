cd36c3fd651dfb63d2d6f57e94900d0d9d4a1f5c
Allie McCarthy <mccarthy.allie@gmail.com>
2019-12-16 19:34:23 -0700

    add DOM tree and imports

---
+++ src/containers/MoviesContainer/MoviesContainer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import MoviesCard from "../../components/MoviesCard/MoviesCard"
+import '../App/App.scss';
+
+const MoviesContainer = () => {
+  return (
+    <section className="moviesContainer">
+      <MoviesCard />
+      //placeholder
+    </section>
+  )
+}
+
+export default MoviesContainer;
