c4de30a223cc936260bc1f642bd0e88f3ad144bc
Kate Malone <katerosemalone@gmail.com>
2019-12-17 15:54:13 -0700

    Adds login fetch post

--- src/util/apiCalls.js
+++ src/util/apiCalls.js
@@ -3,3 +3,18 @@ export const getMovieData = async (url) => {
     const data = await response.json()
     return data;
 }
+
+export const getUser = async (email, password) => {
+    const userLogin = { email: `${email}`, password: `${password}`};
+    const options = {
+        method: POST,
+        body: JSON.stringify(userLogin),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    };
+        
+    const response = await fetch('https://rancid-tomatillos.herokuapp.com/api/v1/login', options)
+    const user = await response.json()
+    return user;
+}
