e39c841dc121ae03bd7859063cb8420833b2dfa6
Allison Wagner <allisonjw01@gmail.com>
2019-12-17 10:54:21 -0700

    Added folder for util and images Co-authored-by: katemalone katerosemalone@gmail.com Co-authored-by: spaceplesiosaur mccarthy.allie@gmail.com

--- src/containers/App/App.js
+++ src/containers/App/App.js
@@ -1,9 +1,9 @@
 import React, { Component } from 'react';
-import LoginForm from '../LoginForm/LoginForm';
+// import LoginForm from '../LoginForm/LoginForm';
 import UserProfile from '../UserProfile/UserProfile';
 import MoviesContainer from '../MoviesContainer/MoviesContainer';
-import ShowPage from '../../components/ShowPage/ShowPage';
-import MovieRatings from '../MovieRatings/MovieRatings';
+// import ShowPage from '../../components/ShowPage/ShowPage';
+// import MovieRatings from '../MovieRatings/MovieRatings';
 import './App.scss';
 
 export default class App extends Component {
@@ -13,37 +13,20 @@ export default class App extends Component {
 
     }
   }
+
+  componentDidMount() {
+    fetch()
+  }
+
   render() {
     return (
       <main className="app-main">
-        <LoginForm />
+        {/* <LoginForm /> */}
         <UserProfile />
         <MoviesContainer />
-        <ShowPage />
-        <MovieRatings />
+        {/* <ShowPage /> */}
+        {/* <MovieRatings /> */}
       </main>
     )
   }
 }
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-//
-// export default App;
