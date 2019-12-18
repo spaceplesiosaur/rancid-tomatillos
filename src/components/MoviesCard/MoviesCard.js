47057e55f4a1d8bccdacfbcba86c2116ca946545
Allison Wagner <allisonjw01@gmail.com>
2019-12-18 12:51:40 -0700

    Adds elements for sytling

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -8,9 +8,9 @@ const MoviesCard = ({ average_rating, id, backdrop_path, overview, poster_path, 
     <section className="moviesContainer-movieCard-cardContainer">
       <h2 className="movieCard-cardContainer-title">{title}</h2>
       <img className="movieCard-cardContainer-poster" src={poster_path}alt="Official movie poster for film"></img>
-      <h4>{average_rating}</h4>
+      <h4>Average Movie Rating: {average_rating}</h4>
       {/* <Link to={'/showpage/'}> */}
-      <button>Add Rating</button>
+      <button className="btn">Add Rating</button>
       {/* </Link> */}
       <MovieRatings />
     </section>
