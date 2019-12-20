b873ecfadf0aa6663142c0599ddce5dd1de3805c
Allison Wagner <allisonjw01@gmail.com>
2019-12-20 16:38:59 -0700

    Add class name to image to add keyframes animation

--- src/components/ShowPage/ShowPage.js
+++ src/components/ShowPage/ShowPage.js
@@ -5,7 +5,7 @@ import '../../containers/App/App.scss';
 const ShowPage = ({movie}) => {
   return (
     <section className="showPage-container">
-    <img src={movie.backdrop_path}></img>
+    <img className="section-showpage-image" src={movie.backdrop_path} alt="movie cover backdrop"></img>
     <h1 className="showPage-container-title">{movie.title}</h1>
     <p className="showPage-container-release">{movie.release_date}</p>
     <p className="showPage-container-overview">{movie.overview}</p>
