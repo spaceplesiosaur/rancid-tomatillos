0e6601826b7107418a82a8feff4a3df89de9dfa5
Allie McCarthy <mccarthy.allie@gmail.com>
2019-12-19 17:10:43 -0700

    Change movies card to a class

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,26 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 // import { Link } from 'react-router-dom';
 import MovieRatings from '../../containers/MovieRatings/MovieRatings'
 import '../../containers/App/App.scss';
 import PropTypes from 'prop-types';
 
-const MoviesCard = ({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) => {
-  return (
-    <section className="moviesContainer-movieCard-cardContainer">
-      <h2 className="movieCard-cardContainer-title">{title}</h2>
-      <img className="movieCard-cardContainer-poster" src={poster_path}alt="Official movie poster for film"></img>
-      <h4>Average Movie Rating: {average_rating}</h4>
-      <button className="movieCard-btn">Add Rating</button>
-      {/* <Link to={'/showpage/'}> */}
-      <button className="movieCard-btn">See More</button>
-      {/* </Link> */}
-      <MovieRatings />
-    </section>
-  )
-
+export default class MoviesCard extends Component {
+  constructor({ average_rating, id, backdrop_path, overview, poster_path, release_date, title}) {
+    super({ average_rating, id, backdrop_path, overview, poster_path, release_date, title})
+    this.state = {
+      showPageOpen: false
+    }
+  }
+  render() {
+    return (
+      <section className="moviesContainer-movieCard-cardContainer">
+        <h2 className="movieCard-cardContainer-title">{title}</h2>
+        <img className="movieCard-cardContainer-poster" src={poster_path}alt="Official movie poster for film"></img>
+        <h4>Average Movie Rating: {average_rating}</h4>
+        <button className="movieCard-btn">Add Rating</button>
+        {/* <Link to={'/showpage/'}> */}
+        <button className="movieCard-btn">See More</button>
+        {/* </Link> */}
+        <MovieRatings />
+      </section>
+    )
+  }
 }
 
-export default MoviesCard;
+
 
 MoviesCard.propTypes = {
   average_rating: PropTypes.number,
