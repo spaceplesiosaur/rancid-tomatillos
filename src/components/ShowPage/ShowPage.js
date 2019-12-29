71a3fec2fd9d99de7647c908d969f14ca2b2f486
Allison Wagner <allisonjw01@gmail.com>
2019-12-29 15:12:31 -0700

    Add span for styling

--- src/components/ShowPage/ShowPage.js
+++ src/components/ShowPage/ShowPage.js
@@ -10,9 +10,9 @@ const ShowPage = ({movie}) => {
     </section>
     <section className="showPage-info-container">  
       <h3 className="showPage-container-title">{movie.title}</h3>
-      <p className="showPage-container-release">Release Date: {movie.release_date}</p>
-      <p className="showPage-container-overview">Synopsis{movie.overview}</p>
-      <p className="showPage-container-averageRating">Average Rating: {movie.average_rating}</p>
+      <p className="showPage-container-release"><span>Release Date:</span>{movie.release_date}</p>
+      <p className="showPage-container-overview"><span>Synopsis:</span>{movie.overview}</p>
+      <p className="showPage-container-averageRating"><span>Average Rating:</span>{movie.average_rating}</p>
       <MovieRatings />
     </section>
     </>
