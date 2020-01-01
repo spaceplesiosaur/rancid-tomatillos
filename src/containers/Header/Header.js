c552f245f71f9ade5d386e59e58d3d932b1a7ad7
Kate Malone <katerosemalone@gmail.com>
2019-12-31 19:29:20 -0800

    Makes styling changes by adding logo images to header

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
+import logo from '../../util/images/tomatoe-logo copy.png';
+import textLogo from '../../util/images/text-logo-RT.png';
 
 export const Home = ({user}) => {
   const username = user.name;
   return(
     <div className="header-div">
-      <h1 className="header-app-name">Rancid Tomatillos</h1>
+      <img className="header-text-logo" src={textLogo} />
+      <img className="logo_image" alt="tomato logo" src={logo} />
       {console.log("USER", user.name)}
      {!user.name ?
-        <NavLink to='/login' className="login-btn">Login</NavLink> :
+        <NavLink to='/login' className="btn login-btn">Login</NavLink> :
         <>
         <NavLink to='/' className="btn logout">Logout</NavLink>
         <h2 className="header-username">{`Hello, ${username}!`}</h2>
