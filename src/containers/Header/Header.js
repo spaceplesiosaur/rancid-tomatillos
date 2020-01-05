3351f03e1323af95ec0855b9eb6771d9bf8ac991
Allie McCarthy <mccarthy.allie@gmail.com>
2020-01-04 18:22:42 -0700

    Add functionality to log user out

--- src/containers/Header/Header.js
+++ src/containers/Header/Header.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { logOut } from '../../actions/index'
 import { NavLink } from 'react-router-dom';
 
-export const Home = ({user}) => {
+export const Header = ({user, logUserOut}) => {
   const username = user.name;
   return(
     <div className="header-div">
@@ -11,7 +12,7 @@ export const Home = ({user}) => {
      {!user.name ?
         <NavLink to='/login' className="login-btn">Login</NavLink> :
         <>
-        <NavLink to='/' className="btn logout">Logout</NavLink>
+        <NavLink to='/' className="btn logout" onClick={logUserOut}>Logout</NavLink>
         <h2 className="header-username">{`Hello, ${username}!`}</h2>
         </> }
     </div>
@@ -23,5 +24,8 @@ export const mapStateToProps = state => ({
     user: state.user
   })
 
+export const mapDispatchToProps = dispatch => ({
+    logUserOut: () => dispatch(logOut())
+})
 
-export default connect(mapStateToProps)(Home)
+export default connect(mapStateToProps, mapDispatchToProps)(Header)
