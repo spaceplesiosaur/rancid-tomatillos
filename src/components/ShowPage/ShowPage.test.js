004586300ce7fb0fbfd49e6ad7cbbd79f8d2d4b8
Allie McCarthy <mccarthy.allie@gmail.com>
2020-01-06 14:24:57 -0700

    Add snapshot tests

--- src/components/ShowPage/ShowPage.test.js
+++ src/components/ShowPage/ShowPage.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from '@testing-library/react';
 import { shallow } from 'enzyme';
-import { ShowPage } from './ShowPage';
+import { ShowPage, mapStateToProps } from './ShowPage';
 
 
 describe('ShowPage', () => {
@@ -11,6 +11,16 @@ describe('ShowPage', () => {
 
     wrapper = shallow(
         <ShowPage
+        movie={{
+            "id": 1,
+            "title": "Jumanji: The Next Level",
+            "poster_path": "https://image.tmdb.org/t/p/original//l4iknLOenijaB85Zyb5SxH1gGz8.jpg",
+            "backdrop_path": "https://image.tmdb.org/t/p/original//zTxHf9iIOCqRbxvl8W5QYKrsMLq.jpg",
+            "release_date": "2019-12-04",
+            "overview": "In Jumanji: The Next Level, the gang is back but the game has changed. As they return to rescue one of their own, the players will have to brave parts unknown from arid deserts to snowy mountains, to escape the world's most dangerous game.",
+            "average_rating": 6.666666666666667
+        }}
+        user={{email: 'diane@turing.io', name: 'Diane', id: 7}}
         />
     )
 
@@ -20,4 +30,21 @@ describe('ShowPage', () => {
     expect(wrapper).toMatchSnapshot()
   })
 
+  describe('mapStateToProps', () => {
+
+    it('should return the user object ', () => {
+      const mockState = {
+        user: {email: 'diane@turing.io', name: 'Diane', id: 7},
+        movies: [{}, {}, {}],
+        ratings: [{}, {}, {}]
+      };
+      const expected = {
+        user: {email: 'diane@turing.io', name: 'Diane', id: 7}
+      };
+
+      const mappedProps = mapStateToProps(mockState);
+
+      expect(mappedProps).toEqual(expected);
+    })
+  })
 })
--- src/containers/MovieRatings/MovieRatings.test.js
+++ src/containers/MovieRatings/MovieRatings.test.js
@@ -11,6 +11,34 @@ describe('MovieRatings', () => {
 
     wrapper = shallow(
         <MovieRatings
+        movieId={2}
+        rating={2}
+        user={{id: 7, name: 'Diana', email: "diana@turing.io"}}
+        allRatings={[{
+            "id": 118,
+            "user_id": 7,
+            "movie_id": 20,
+            "rating": 2,
+            "created_at": "2020-01-02T23:21:56.186Z",
+            "updated_at": "2020-01-02T23:21:56.186Z"
+        },
+        {
+            "id": 108,
+            "user_id": 7,
+            "movie_id": 14,
+            "rating": 4,
+            "created_at": "2019-12-30T21:29:51.243Z",
+            "updated_at": "2019-12-30T21:29:51.243Z"
+        },
+        {
+            "id": 110,
+            "user_id": 7,
+            "movie_id": 18,
+            "rating": 5,
+            "created_at": "2019-12-31T21:39:21.783Z",
+            "updated_at": "2019-12-31T21:39:21.783Z"
+        }]}
+        deleteRating={jest.fn()}
         />
     )
 
